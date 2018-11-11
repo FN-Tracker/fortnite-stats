dbf561bcab6147744e1cf7964f0651cc2be98e84
Philip <38354850+philipmoisen@users.noreply.github.com>
2018-11-11 16:42:45 +0100

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,11 @@
+echo "Now this will install all the npm things"  
+
+npm install body-parser@1.18.3
+
+npm install express@4.16.3
+
+npm install path@0.12.7
+
+npm install request@2.87.0
+
+echo "Done you are redy to start up just run the test file"
